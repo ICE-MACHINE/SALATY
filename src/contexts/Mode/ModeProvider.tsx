9053e091c29fdf755b433034b01efda25ba7b5a5
THE MACHINE <a.Matib@univ-dbkm.dz>
2025-07-30 00:38:51 +0100

    edit the logic of the mode

--- src/contexts/Mode/ModeProvider.tsx
+++ src/contexts/Mode/ModeProvider.tsx
@@ -1,15 +1,15 @@
 import ModeContext from "./ModeContext.tsx";
 import type { ReactNode } from "react";
 import { useState, useEffect } from "react";
-
+type ModeType = 'light' | 'dark';
 export default function ModeProvider({ children }: { children: ReactNode }) {
-  const [mode, setMode] = useState(()=>{
-    return localStorage.getItem("mode") || "light";
+  const [mode, setMode] = useState<ModeType>(() => {
+    return (localStorage.getItem("mode") as ModeType) || "light";
   });
 
   useEffect(() => {
     const savedMode = localStorage.getItem("mode");
-    if (savedMode) {
+    if (savedMode === "light" || savedMode === "dark") {
       setMode(savedMode);
     }
   }, []);
